52d35ef4f7ce2537533cb371d08b26921b35e028
Koki Shinjo <sktometometo@gmail.com>
2023-04-20 12:58:54 +0900

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
+import os
 from distutils.core import setup
 
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=['LAVIS_grpc_server'],
-                             package_dir={'': 'src'})
+
+def get_requirements():
+  lib_folder = os.path.dirname(os.path.realpath(__file__))
+  requirement_path = lib_folder + '/requirements.txt'
+  install_requires = [
+  ]  # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+  if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+      install_requires = f.read().splitlines()
+  return install_requires
+
+
+d = generate_distutils_setup(
+    packages=['LAVIS_grpc_server'],
+    package_dir={'': 'src'},
+    install_requires=get_requirements(),
+    entry_points={
+        'console_scripts': [
+            "download_model_cache = LAVIS_grpc_server:download_model_cache",
+        ],
+        'gui_scripts': [
+            "run_lavis_server = LAVIS_grpc_server:main_server",
+            "sample_lavis_client = LAVIS_grpc_server:main_client_sample"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])
 
 setup(**d)
