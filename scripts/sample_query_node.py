8bd243bc9f41cc0c05ac74e6e5329c00b9f346d2
Koki Shinjo <sktometometo@gmail.com>
2023-04-21 18:58:42 +0900

    Update demo

--- scripts/sample_query_node.py
+++ scripts/sample_query_node.py
@@ -4,6 +4,7 @@ import logging
 
 import rospy
 from sensor_msgs.msg import Image
+from sound_play.libsoundplay import SoundClient
 
 from LAVIS_grpc_server.srv import ImageCaptioning, ImageCaptioningRequest
 
@@ -13,6 +14,7 @@ class SampleQueryNode:
   def __init__(self):
 
     rospy.wait_for_service('~image_captioning')
+    self.sound_client = SoundClient()
     self.client = rospy.ServiceProxy('~image_captioning', ImageCaptioning)
     self.sub = rospy.Subscriber('~image', Image, self.callback)
     rospy.loginfo('Initialized.')
@@ -23,6 +25,7 @@ class SampleQueryNode:
     req.image = msg
     res = self.client(req)
     rospy.loginfo(res.caption)
+    self.sound_client.say(res.caption, blocking=True)
 
 
 if __name__ == '__main__':
